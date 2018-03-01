dbe3815ba384d249926914c5235a697e93f2a520
Sam Standing <samzstanding@gmail.com>
2018-03-01 16:56:56 -0600

    built get employee function

--- server/public/services/CustomService.js
+++ server/public/services/CustomService.js
@@ -6,7 +6,13 @@ app.service('CustomService', function(){
 
     self.getEmployees = function () {
         $http({
-            method:''
+            method:'GET',
+            url:'/hr'
+        }).then(function (response) {
+            console.log(response.data);
+            self.employeeArray.list = response.data;
+        }).catch(function (error) {
+            console.log('error on get: ', error);
         })
     }
 
